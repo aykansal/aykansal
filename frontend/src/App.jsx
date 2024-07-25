e5f0559ba2d913e9a417084fdbb8c7ff31f9bf33
Ayush <ayushkansal303@gmail.com>
2024-07-25 16:25:30 +0530

    fixed whitespace issue for mobiles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,11 @@ import { Navbar } from "./components";
 
 function App() {
   return (
-    <div className="bg-slate-900 min-h-screen max-w-screen flex justify-center ">
+    <div
+      className={`bg-slate-900 min-h-screen max-w-screen ${
+        window.innerWidth > 768 && "flex justify-center"
+      }`}
+    >
       {/* <Navbar /> */}
       <div className="lg:flex justify-between gap-10 xl:gap-x-7 2xl:gap-x-0 justify-center max-w-[1536px] text-slate-400 py-[4rem] xl:py-[6rem] px-[6rem] xs:px-[6rem] sm:px-[6rem] md:px-[6rem] lg:px-[3.5rem] xl:px-[2rem] 2xl:px-[8rem]">
         <div className="basis-[45%] 2xl:basis-1/2  ">
